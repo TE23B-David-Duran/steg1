97997657720efdcb686668ea8c214dfa937bc1e1
David Duran <david.duranherrera@elev.ga.ntig.se>
2024-11-13 15:01:51 +0100

    fefefe

--- steg1/Program.cs
+++ steg1/Program.cs
@@ -1,3 +1,14 @@
-﻿if (6 >= 3) {
-    Console.WriteLine("Hello World!");
+﻿//if (6 >= 3) {
+  //  Console.WriteLine("Hello World!");
+// }
+
+Console.WriteLine("David");
+
+string name = Console.ReadLine();
+
+if(name == "Kalleanka"){
+    Console.WriteLine("Welcome!");
+    //använder namn "Daivd" är = Kalleanka    då det kommer "Welcomer!"
 }
+
+Console.ReadLine();
