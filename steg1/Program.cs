3811fa7aa5f2a66e70a89186afe9f14277cacecc
David Duran <david.duranherrera@elev.ga.ntig.se>
2024-11-20 15:02:59 +0100

    derftgygh

--- steg1/Program.cs
+++ steg1/Program.cs
@@ -1,14 +1,45 @@
-﻿//if (6 >= 3) {
-  //  Console.WriteLine("Hello World!");
-// }
+﻿// Uppgift 1 
 
-Console.WriteLine("David");
+//if (6 >= 3) {
+ // Console.WriteLine("Hello World!");
+ //}
 
-string name = Console.ReadLine();
+ //Console.WriteLine("David");
 
-if(name == "Kalleanka"){
-    Console.WriteLine("Welcome!");
-    //använder namn "Daivd" är = Kalleanka    då det kommer "Welcomer!"
+
+// Uppgift 2 och 3 
+//string password = "12345";
+//string name = "Kalleanka";
+//Console.WriteLine("Write your username");
+
+//string answername = Console.ReadLine();
+
+
+//Console.WriteLine("Write your password");
+//string answerpassword = Console.ReadLine();
+
+
+//if(name == answername && password == answerpassword)
+//{
+//  Console.WriteLine("Welcome!");
+//}
+//else //
+//{
+//  Console.WriteLine("Wrong password or username!");
+//}
+
+//Console.ReadLine();
+
+
+
+// Uppgift 4 
+string Hello = "Hello, World";
+for (int i = 0; i < 32; i++)
+{
+  Console.WriteLine(Hello);
 }
-
 Console.ReadLine();
+
+
+// Uppgift 5
+
